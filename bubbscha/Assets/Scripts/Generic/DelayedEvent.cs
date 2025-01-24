ec20cf323650737710468d6d2bbb15ffe0a1810a
JakobMueller <50182067+JakobMueller@users.noreply.github.com>
2025-01-25 00:42:29 +0100

    Add some generic scripts

---
+++ bubbscha/Assets/Scripts/Generic/DelayedEvent.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Common
+{
+    public class DelayedEvent : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _event;
+        [SerializeField] private float _delay;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        /// <summary>
+        /// Invokes the UnityEvent after a specified delay.
+        /// </summary>
+        public async void InvokeDelayed()
+        {
+            Cancel();
+            
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
+            try
+            {
+                await Task.Delay((int)(_delay * 1000), token);
+                
+                if (!token.IsCancellationRequested)
+                {
+                    _event?.Invoke();
+                }
+            }
+            catch (TaskCanceledException)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Cancels any ongoing delayed invocation.
+        /// </summary>
+        private void Cancel()
+        {
+            if (_cancellationTokenSource == null) return;
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+            _cancellationTokenSource = null;
+        }
+    }
+}
---
+++ bubbscha/Assets/Scripts/Generic/TriggerEventOnEnable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Common
+{
+    public class TriggerEventOnEnable : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _event;
+
+        private void OnEnable()
+        {
+            _event.Invoke();
+        }
+    }
+}
