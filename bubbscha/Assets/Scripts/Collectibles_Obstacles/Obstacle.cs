c25d39cb0cfec37de22d579858f19d60e46276ef
Korvek <e.gattermann@li-ga.de>
2025-01-25 16:32:21 +0100

    ADD: GameManager

--- bubbscha/Assets/Scripts/Collectibles_Obstacles/Obstacle.cs
+++ bubbscha/Assets/Scripts/Collectibles_Obstacles/Obstacle.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ObstacleBalance : MonoBehaviour
+public class Obstacle : MonoBehaviour
 {
     [SerializeField] float force = 1f;
     //[SerializeField] float radius =1f;
     //[SerializeField] float upward = 1f;
-    UnityEvent<int> collision;
+    UnityEvent<float> collision;
     private void OnTriggerEnter(Collider other)
     {
         GetComponent<Collider>().enabled = false;
+        collision.Invoke(force);
         GetComponent<Rigidbody>().AddForce(new Vector3(0, 5, 0));
         //other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius, upward);
     }
