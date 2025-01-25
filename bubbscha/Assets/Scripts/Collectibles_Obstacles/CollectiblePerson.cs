57835c55471634f41605b3aa1c34ba1a35d1ae17
Korvek <e.gattermann@li-ga.de>
2025-01-25 15:46:17 +0100

    ADD: Score increase over time + multiplicator

--- bubbscha/Assets/Scripts/Collectibles_Obstacles/CollectiblePerson.cs
+++ bubbscha/Assets/Scripts/Collectibles_Obstacles/CollectiblePerson.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectibleScore : MonoBehaviour
+public class CollectiblePerson : MonoBehaviour
 {
-    [Tooltip("Number added to the score")]
-    [SerializeField] int scoreBonus;
+    [Tooltip("Number of people picked up")]
+    [SerializeField] int people=1;
 
     private void OnTriggerEnter(Collider other)
     {
         //Maybe identifier for player
         //if (other.CompareTag("Player"))
         //{
-        GameStats.instance.ChangeScore(scoreBonus);
+        GameStats.instance.ChangePeople(people);
         Renderer.Destroy(gameObject);
         //}
     }
--- bubbscha/Assets/Scripts/Generic/GameStats.cs
+++ bubbscha/Assets/Scripts/Generic/GameStats.cs
@@ -4,31 +4,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/*
+ * Highscore over time
+ * people multiplicator
+ * people as lives
+ */
+
 public class GameStats : MonoBehaviour
 {
     public static GameStats instance;
     [Tooltip("Number for passenger mood")]
-    [SerializeField][Range(0, 1)] float mood =1f;
+    [SerializeField][Range(0, 1)] float mood = 1f;
     [Tooltip("Number for collected points")]
-    [SerializeField] int score;
+    [SerializeField] int score = 0;
+    [Tooltip("Increase of points per second")]
+    [SerializeField] int scoreOverTime = 10;
+    [Tooltip("Decrase of mood per second")]
+    [SerializeField] float moodOverTime = -0.01f;
+    [Tooltip("Number for collected people")]
+    [SerializeField] int people = 1;
     public UnityEvent<float> moodChanged;
-    public UnityEvent scoreChanged;
+    public UnityEvent<int> scoreChanged;
+    public UnityEvent peopleChanged;
 
     private void Awake()
     {
         instance = this;
         StartCoroutine(moodDecrease());
+        StartCoroutine(scoreIncrease());
     }
-    
+
     public void ChangeMood(float moodBonus)
     {
-        mood += moodBonus;
+        mood += moodBonus * people;
+        if (mood <= 0f)
+        {
+            ChangePeople(-1);
+            if (people <= 0)
+            {
+                //TODO: End Game, Show Scoreboard
+            }
+        }
         moodChanged.Invoke(mood);
     }
     public void ChangeScore(int scoreBonus)
     {
         score += scoreBonus;
-        scoreChanged.Invoke();
+        scoreChanged.Invoke(score);
+    }
+    public void ChangePeople(int change)
+    {
+        people += change;
+        peopleChanged.Invoke();
     }
 
     IEnumerator moodDecrease()
@@ -36,7 +63,15 @@ public class GameStats : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(1);
-            ChangeMood(-0.01f);
+            ChangeMood(moodOverTime);
+        }
+    }
+    IEnumerator scoreIncrease()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            ChangeScore(scoreOverTime * people);
         }
     }
 }
