85c0771d008a846622eab7b04bf1ac69142fb0d3
Korvek <e.gattermann@li-ga.de>
2025-01-25 14:11:55 +0100

    ADD: Obstacles fly away and deactivate

--- bubbscha/Assets/Scripts/Collectibles_Obstacles/ObstacleBalance.cs
+++ bubbscha/Assets/Scripts/Collectibles_Obstacles/ObstacleBalance.cs
@@ -9,11 +9,16 @@ public class ObstacleBalance : MonoBehaviour
     [SerializeField] float upward = 1f;
     private void OnCollisionEnter(Collision collision)
     {
-        //GetComponent<Rigidbody>().AddRelativeForce();
+        GetComponent<Collider>().enabled = false;
+        GetComponent<Rigidbody>().AddForce( new Vector3(0,5,0));
         //(collision.body as Rigidbody).AddExplosionForce(force,transform.position,radius,upward);
     }
     private void OnTriggerEnter(Collider other)
     {
         //other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius, upward);
+    }
+    private void OnBecameInvisible()
+    {
+        Renderer.Destroy (gameObject);
     }
 }
