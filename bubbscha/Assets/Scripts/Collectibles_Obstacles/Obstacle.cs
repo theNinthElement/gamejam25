ab068245b54f7690e82f5ab28878d7a675695b63
JakobMueller <50182067+JakobMueller@users.noreply.github.com>
2025-01-26 16:16:24 +0100

    add cooldown to obstacles

--- bubbscha/Assets/Scripts/Collectibles_Obstacles/Obstacle.cs
+++ bubbscha/Assets/Scripts/Collectibles_Obstacles/Obstacle.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class Obstacle : MonoBehaviour
 {
@@ -9,17 +11,22 @@ public class Obstacle : MonoBehaviour
     //[SerializeField] float radius =1f;
     //[SerializeField] float upward = 1f;
     public UnityEvent<float> collision;
+    private float _cooldown;
+
     private void OnTriggerEnter(Collider other)
     {
+        if (Time.time < _cooldown) return;
         bool left = Random.value <= 0.5f;
         if (left)
         {
             Debug.Log("Left");
+            _cooldown = Time.time + 1;
             GameManager.instance.PlayerCollision(-force);
         }
         else
         {
             Debug.Log("Right");
+            _cooldown = Time.time + 1;
             GameManager.instance.PlayerCollision(force);
         }
     }
